a01dfef77f30fbb440f6e91035ca0973540f4d0e
Pranshu Indurkhya <pranshuindurkhya@gmail.com>
2020-11-07 10:40:31 +0530

    changes in main class

--- src/com/indu/Main.java
+++ src/com/indu/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("Pranshu Indurkhya");
+        System.out.println("Hi World");
             }
 }
