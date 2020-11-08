5bec5ef264d2ac45f87d004dfbdaef587a1c1d24
Pranshu Indurkhya <pranshuindurkhya@gmail.com>
2020-11-08 15:50:14 +0530

    changes in Employee class.

--- src/com/indu/model/Employee.java
+++ src/com/indu/model/Employee.java
@@ -4,10 +4,20 @@ public class Employee {
 
     private String name;
     private int age;
+    private  int salary;
 
-    public Employee(String name, int age) {
+    public Employee(String name, int age, int salary) {
         this.name = name;
         this.age = age;
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
     }
 
     public String getName() {
